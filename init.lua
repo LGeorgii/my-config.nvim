d92099cd87eff61edda994c5b4cec87da161a4c7
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-08 21:32:12 +0200

    Removed "nvim-web-devicons" from main config.

--- init.lua
+++ init.lua
@@ -598,10 +598,6 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'nvim-tree/nvim-web-devicons',
-  },
-
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is
