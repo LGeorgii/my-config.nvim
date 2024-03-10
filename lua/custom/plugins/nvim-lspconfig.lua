54d7e6172eb1f5ee267255ed1802d2ae8cbf7a81
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-11 00:03:00 +0200

    Made buildServer.json hidden.

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -72,7 +72,7 @@ return {
         -- "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp",
       },
       root_dir = function(filename, _)
-        return util.root_pattern("buildServer.json")(filename)
+        return util.root_pattern(".buildServer.json")(filename)
             or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
             or util.find_git_ancestor(filename)
             or util.root_pattern("Package.swift")(filename)
