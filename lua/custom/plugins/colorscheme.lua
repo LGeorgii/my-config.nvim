ba329afdfed795e892e170baf86e221f5d626ac2
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-10 13:03:18 +0200

    Set colorscheme.

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,5 +1,8 @@
 return {
-  'ayu',
-  mirage = true, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+  'Shatur/neovim-ayu',
+  mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
   overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+  config = function()
+    require('ayu').colorscheme()
+  end
 }
