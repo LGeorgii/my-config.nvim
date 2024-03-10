abd4c1ef20d583a976278bf26aca791466590420
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-10 20:14:15 +0200

    Deleted spaces in options.

--- lua/custom/core/options.lua
+++ lua/custom/core/options.lua
@@ -1,33 +1,20 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-
 vim.g.have_nerd_font = true
-
 vim.opt.mouse = 'a'
-
 vim.opt.showmode = false
-
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.undofile = true
-
 vim.opt.breakindent = true
-
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
 vim.opt.signcolumn = 'yes'
-
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
-
 vim.opt.splitright = true
 vim.opt.splitbelow = true
-
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
 vim.opt.inccommand = 'split'
-
 vim.opt.scrolloff = 15
-
 vim.opt.hlsearch = true
