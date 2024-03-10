664cdab19c061352dfc409194e8d2e2dcf23195d
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-10 15:40:55 +0200

    Deleted comment in conform plugin.

--- lua/custom/plugins/conform-nvim.lua
+++ lua/custom/plugins/conform-nvim.lua
@@ -1,5 +1,4 @@
 return {
-  -- Autoformat
   'stevearc/conform.nvim',
   opts = {
     notify_on_error = false,
@@ -9,12 +8,6 @@ return {
     },
     formatters_by_ft = {
       lua = { 'stylua' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use a sub-list to tell conform to run *until* a formatter
-      -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
     },
   },
 }
