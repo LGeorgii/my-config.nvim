01619eccee79594f8b1db9156ee1ce207aa6ca93
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-10 15:28:11 +0200

    Fixed color scheme to show different backgrounds on active and non-active windows.

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -4,6 +4,16 @@ return {
   overrides = {},
 
   config = function()
+    require('ayu').setup({
+      overrides = function()
+        if vim.o.background == 'dark' then
+          return { NormalNC = {bg = '#0f151e', fg = '#808080'} }
+        else
+          return { NormalNC = {bg = '#f0f0f0', fg = '#808080'} }
+        end
+      end
+    })
+
     require('ayu').colorscheme()
   end
 }
