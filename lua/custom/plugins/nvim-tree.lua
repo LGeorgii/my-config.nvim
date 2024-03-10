92bb35e754f2d302b2ca92fca9d3259e142434df
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-10 19:35:28 +0200

    Revisited nvim-tree.

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,26 +1,24 @@
 return {
   "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons" -- Provides icons and colors for each icon.
+  },
   config = function()
     local nvimtree = require("nvim-tree")
 
-    -- recommended settings from nvim-tree documentation
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
-    -- change color for arrows in tree to light blue
     vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 
-    -- configure nvim-tree
     nvimtree.setup({
       notify = {
         threshold = vim.log.levels.WARN,
       },
       view = {
-        width = 40,
+        width = 50,
         relativenumber = true,
       },
-      -- change folder arrow icons
       renderer = {
         indent_markers = {
           enable = true,
@@ -28,15 +26,13 @@ return {
         icons = {
           glyphs = {
             folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
+              arrow_closed = "",
+              arrow_open = "",
             },
           },
         },
       },
-      -- disable window_picker for
-      -- explorer to work well with
-      -- window splits
+      -- Disable window_picker for explorer to work well with window splits.
       actions = {
         open_file = {
           quit_on_open = true,
@@ -57,7 +53,6 @@ return {
       },
     })
 
-    -- set keymaps
     local keymap = vim.keymap
 
     keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
