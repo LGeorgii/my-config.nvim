3d29886bc364cf14f3158077aec6f682d9f38c7d
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-11 19:22:42 +0200

    Reconfigured nvim-cmp.

--- lua/custom/plugins/nvim-cmp.lua
+++ lua/custom/plugins/nvim-cmp.lua
@@ -1,54 +1,58 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
+  'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
   dependencies = {
-    "hrsh7th/cmp-buffer",           -- Source for text in buffer.
-    "hrsh7th/cmp-path",             -- Source for file system paths.
-    "L3MON4D3/LuaSnip",             -- Snippet engine.
-    "saadparwaiz1/cmp_luasnip",     -- Luasnip completion source for nvim-cmp.
-    "rafamadriz/friendly-snippets", -- Useful snippets.
-    "onsails/lspkind.nvim",         -- VS-code like pictograms.
+    {
+      'L3MON4D3/LuaSnip', -- Snippet Engine.
+      build = (function()
+        -- Build Step is needed for regex support in snippets.
+        -- This step is not supported in many windows environments.
+        -- Remove the below condition to re-enable on windows.
+        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+          return
+        end
+
+        return 'make install_jsregexp'
+      end)(),
+    },
+    'saadparwaiz1/cmp_luasnip', -- LuaSnip completion source for nvim-cmp.
+    'hrsh7th/cmp-nvim-lsp', -- nvim-cmp source for neovim's built-in language server client.
+    'hrsh7th/cmp-path', -- nvim-cmp source for filesystem paths.
+    'rafamadriz/friendly-snippets', -- Snippets collection for a set of different programming languages.
   },
   config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    local lspkind = require("lspkind")
+    local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
 
-    require("luasnip.loaders.from_vscode").lazy_load()
+    luasnip.config.setup {}
 
-    cmp.setup({
-      completion = {
-        completeopt = "menu,menuone,preview",
-      },
+    cmp.setup {
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
       },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
+      completion = { completeopt = 'menu,menuone,noinsert' },
+      mapping = cmp.mapping.preset.insert {
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-y>'] = cmp.mapping.confirm { select = true },
+        ['<C-l>'] = cmp.mapping(function()
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          end
+        end, { 'i', 's' }),
+        ['<C-h>'] = cmp.mapping(function()
+          if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          end
+        end, { 'i', 's' }),
       },
-      mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<C-y>"] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace }),
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = 'buffer' },
-      }),
-      formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
-        }),
+      sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'path' },
       },
-    })
+    }
   end,
 }
