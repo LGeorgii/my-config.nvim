9275cd7af36617db8f11424f2dd5f9722d6188f1
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-11 09:25:51 +0200

    Added Mason to LSP config.

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -1,11 +1,14 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
+  'neovim/nvim-lspconfig',
+  event = { 'BufReadPre', 'BufNewFile' },
   dependencies = {
-    "hrsh7th/cmp-nvim-lsp",                  -- nvim-cmp source for neovim's built-in language server client.
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    'hrsh7th/cmp-nvim-lsp', -- nvim-cmp source for neovim's built-in language server client.
     {
-      "antosha417/nvim-lsp-file-operations", -- Adds support for file operations using built-in LSP support.
-      config = true
+      'antosha417/nvim-lsp-file-operations', -- Adds support for file operations using built-in LSP support.
+      config = true,
     },
   },
   config = function()
@@ -41,49 +44,90 @@ return {
       end,
     })
 
-    local lspconfig = require("lspconfig")
-    local util = require("lspconfig.util")
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-    local capabilities = cmp_nvim_lsp.default_capabilities()
+    local lspconfig = require 'lspconfig'
+    local util = require 'lspconfig.util'
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-    lspconfig["lua_ls"].setup({
-      settings = {
-        Lua = {
-          runtime = { version = 'LuaJIT' },
-          workspace = {
-            checkThirdParty = false,
-            library = {
-              '${3rd}/luv/library',
-              unpack(vim.api.nvim_get_runtime_file('', true)),
+    local servers = {
+      -- clangd = {},
+      -- gopls = {},
+      -- pyright = {},
+      -- rust_analyzer = {},
+      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+      --
+      -- Some languages (like typescript) have entire language plugins that can be useful:
+      --    https://github.com/pmizio/typescript-tools.nvim
+      --
+      -- But for many setups, the LSP (`tsserver`) will work just fine
+      -- tsserver = {},
+
+      lua_ls = {
+        settings = {
+          Lua = {
+            runtime = { version = 'LuaJIT' },
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                '${3rd}/luv/library',
+                unpack(vim.api.nvim_get_runtime_file('', true)),
+              },
             },
-          },
-          completion = {
-            callSnippet = 'Replace',
+            completion = {
+              callSnippet = 'Replace',
+            },
+            diagnostics = { disable = { 'missing-fields' } },
           },
         },
       },
+    }
+
+    require('mason').setup()
+
+    local ensure_installed = vim.tbl_keys(servers or {})
+
+    vim.list_extend(ensure_installed, {
+      'stylua', -- Used to format lua code.
     })
 
-    lspconfig["sourcekit"].setup({
+    require('mason-tool-installer').setup {
+      ensure_installed = ensure_installed,
+    }
+
+    require('mason-lspconfig').setup {
+      handlers = {
+        function(server_name)
+          local server = servers[server_name] or {}
+          -- This handles overriding only values explicitly passed
+          -- by the server configuration above. Useful when disabling
+          -- certain features of an LSP (for example, turning off formatting for tsserver)
+          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+          require('lspconfig')[server_name].setup(server)
+        end,
+      },
+    }
+
+    -- SourceKit is configured separately because it's not supported by Mason plugin.
+    lspconfig['sourcekit'].setup {
       capabilities = capabilities,
       cmd = {
-        vim.trim(vim.fn.system("xcrun -f sourcekit-lsp")),
+        vim.trim(vim.fn.system 'xcrun -f sourcekit-lsp'),
         -- In case of issues try using the full path to sourcekit-lsp:
         -- "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp",
       },
       root_dir = function(filename, _)
-        return util.root_pattern("buildServer.json")(filename)
-            or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
-            or util.find_git_ancestor(filename)
-            or util.root_pattern("Package.swift")(filename)
+        return util.root_pattern 'buildServer.json'(filename)
+          or util.root_pattern('*.xcodeproj', '*.xcworkspace')(filename)
+          or util.find_git_ancestor(filename)
+          or util.root_pattern 'Package.swift'(filename)
       end,
-    })
+    }
 
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
 
     for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      local hl = 'DiagnosticSign' .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
     end
   end,
 }
