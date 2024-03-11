8684858c9189a43edb08e7f41323c98cd41612bf
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-11 19:35:01 +0200

    Reconfigured lspconfig.

--- lua/custom/plugins/conform-nvim.lua
+++ lua/custom/plugins/conform-nvim.lua
@@ -8,7 +8,7 @@ return {
     },
     formatters_by_ft = {
       lua = { 'stylua' },
-      javascript = { { 'prettier' } },
+      javascript = { 'prettier' },
     },
   },
 }
--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -1,15 +1,10 @@
 return {
   'neovim/nvim-lspconfig',
-  event = { 'BufReadPre', 'BufNewFile' },
   dependencies = {
+    -- Automatically install LSPs and related tools to stdpath for neovim
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
-    'hrsh7th/cmp-nvim-lsp', -- nvim-cmp source for neovim's built-in language server client.
-    {
-      'antosha417/nvim-lsp-file-operations', -- Adds support for file operations using built-in LSP support.
-      config = true,
-    },
   },
   config = function()
     vim.api.nvim_create_autocmd('LspAttach', {
@@ -19,17 +14,48 @@ return {
           vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
+        -- Jump to the definition of the word under your cursor.
+        --  This is where a variable was first declared, or where a function is defined, etc.
+        --  To jump back, press <C-T>.
         map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+        -- Find references for the word under your cursor.
         map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+        -- Jump to the implementation of the word under your cursor.
+        --  Useful when your language has ways of declaring types without an actual implementation.
         map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+        -- Jump to the type of the word under your cursor.
+        --  Useful when you're not sure what type a variable is and you want to see
+        --  the definition of its *type*, not where it was *defined*.
         map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+
+        -- Fuzzy find all the symbols in your current document.
+        --  Symbols are things like variables, functions, types, etc.
+        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+
+        -- Fuzzy find all the symbols in your current workspace
+        --  Similar to document symbols, except searches over your whole project.
+        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+        -- Rename the variable under your cursor
+        --  Most Language Servers support renaming across files, etc.
         map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+        -- Execute a code action, usually your cursor needs to be on top of an error
+        -- or a suggestion from your LSP for this to activate.
         map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+        -- Opens a popup that displays documentation about the word under your cursor
+        --  See `:help K` for why this keymap
         map('K', vim.lsp.buf.hover, 'Hover Documentation')
+
+        -- WARN: This is not Goto Definition, this is Goto Declaration.
+        --  For example, in C this would take you to the header
         map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-
         if client and client.server_capabilities.documentHighlightProvider then
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
@@ -44,24 +70,11 @@ return {
       end,
     })
 
-    local lspconfig = require 'lspconfig'
-    local util = require 'lspconfig.util'
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
     local servers = {
-      -- clangd = {},
-      -- gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
-      -- tsserver = {},
-
+      tsserver = {},
       lua_ls = {
         settings = {
           Lua = {
@@ -86,48 +99,16 @@ return {
 
     local ensure_installed = vim.tbl_keys(servers or {})
 
-    vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format lua code.
-    })
-
-    require('mason-tool-installer').setup {
-      ensure_installed = ensure_installed,
-    }
+    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
     require('mason-lspconfig').setup {
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for tsserver)
           server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
           require('lspconfig')[server_name].setup(server)
         end,
       },
     }
-
-    -- SourceKit is configured separately because it's not supported by Mason plugin.
-    lspconfig['sourcekit'].setup {
-      capabilities = capabilities,
-      cmd = {
-        vim.trim(vim.fn.system 'xcrun -f sourcekit-lsp'),
-        -- In case of issues try using the full path to sourcekit-lsp:
-        -- "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp",
-      },
-      root_dir = function(filename, _)
-        return util.root_pattern 'buildServer.json'(filename)
-          or util.root_pattern('*.xcodeproj', '*.xcworkspace')(filename)
-          or util.find_git_ancestor(filename)
-          or util.root_pattern 'Package.swift'(filename)
-      end,
-    }
-
-    local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
-
-    for type, icon in pairs(signs) do
-      local hl = 'DiagnosticSign' .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-    end
   end,
 }
