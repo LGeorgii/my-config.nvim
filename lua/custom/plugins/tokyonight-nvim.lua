6c9d1524c08645b87ce6cf1690f5ee782ad8b3ca
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-13 09:51:24 +0200

    Disabled italics in TokyoNight colorscheme.

--- lua/custom/plugins/tokyonight-nvim.lua
+++ lua/custom/plugins/tokyonight-nvim.lua
@@ -2,6 +2,15 @@ return {
   'folke/tokyonight.nvim',
   priority = 1000,
   init = function()
+    require('tokyonight').setup {
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+        functions = {},
+        variables = {},
+      },
+    }
+
     vim.cmd.colorscheme 'tokyonight-night'
     vim.cmd.hi 'Comment gui=none'
   end,
