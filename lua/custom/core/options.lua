bb9166b32a670802811df6019408f074554959e1
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-14 20:57:53 +0200

    Re-aranged options.

--- lua/custom/core/options.lua
+++ lua/custom/core/options.lua
@@ -9,13 +9,13 @@ vim.opt.showmode = false
 
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.undofile = true
-vim.opt.breakindent = true
 
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.breakindent = true
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
