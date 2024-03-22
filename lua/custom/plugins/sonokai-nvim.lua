4c3d2304ce273fcf03fa955a3b8e04814718d7fc
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-22 23:37:27 +0200

    Disable Sonokai theme

--- lua/custom/plugins/sonokai-nvim.lua
+++ lua/custom/plugins/sonokai-nvim.lua
@@ -2,13 +2,13 @@ return {
   'sainnhe/sonokai',
   priority = 1000,
   init = function()
-    if vim.fn.has 'termguicolors' == 1 then
-      vim.o.termguicolors = true
-    end
-
-    vim.g.sonokai_style = 'atlantis'
-    vim.g.sonokai_better_performance = 1
-    vim.g.sonokai_transparent_background = 1
-    vim.cmd 'colorscheme sonokai'
+    -- if vim.fn.has 'termguicolors' == 1 then
+    --   vim.o.termguicolors = true
+    -- end
+    --
+    -- vim.g.sonokai_style = 'atlantis'
+    -- vim.g.sonokai_better_performance = 1
+    -- vim.g.sonokai_transparent_background = 1
+    -- vim.cmd 'colorscheme sonokai'
   end,
 }
