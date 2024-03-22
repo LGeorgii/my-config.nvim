0a0723530f137bbf1ca67df3f06ed02667b9241c
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-22 09:11:00 +0200

    Made tokyonight transparent and set terminal background color to tokyonight default bg color to remove paddings and for future transparency settings

--- lua/custom/plugins/tokyonight-nvim.lua
+++ lua/custom/plugins/tokyonight-nvim.lua
@@ -5,38 +5,33 @@ return {
     require('tokyonight').setup {
       on_highlights = function(hl, c)
         hl.TelescopeNormal = {
-          bg = c.bg_dark,
           fg = c.fg_dark,
         }
         hl.TelescopeBorder = {
-          bg = c.bg_dark,
           fg = '#82A0F1',
         }
-        hl.TelescopePromptNormal = {
-          bg = c.bg_dark,
-        }
+        hl.TelescopePromptNormal = {}
         hl.TelescopePromptBorder = {
-          bg = c.bg_dark,
           fg = '#82A0F1',
         }
         hl.TelescopePromptTitle = {
-          bg = c.bg_dark,
           fg = '#82A0F1',
         }
         hl.TelescopePreviewTitle = {
-          bg = c.bg_dark,
           fg = '#82A0F1',
         }
         hl.TelescopeResultsTitle = {
-          bg = c.bg_dark,
           fg = '#82A0F1',
         }
       end,
+      transparent = true,
       styles = {
         comments = { italic = false },
         keywords = { italic = false },
         functions = {},
         variables = {},
+        sidebars = 'transparent',
+        floats = 'transparent',
       },
     }
 
