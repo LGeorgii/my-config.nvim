2b5fc836243837e6eead2c0334d7418a93f1dfde
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-22 22:27:24 +0200

    Added new Sonokai theme.

--- lua/custom/plugins/tokyonight-nvim.lua
+++ lua/custom/plugins/tokyonight-nvim.lua
@@ -2,32 +2,32 @@ return {
   'folke/tokyonight.nvim',
   priority = 1000,
   init = function()
-    require('lspconfig.ui.windows').default_options.border = 'rounded'
-
-    require('tokyonight').setup {
-      on_highlights = function(hl)
-        hl.TelescopeBorder = {
-          fg = '#82A0F1',
-        }
-        hl.TelescopePromptBorder = {
-          fg = '#82A0F1',
-        }
-        hl.LspInfoBorder = {
-          fg = '#82A0F1',
-        }
-      end,
-      -- transparent = true,
-      styles = {
-        comments = { italic = false },
-        keywords = { italic = false },
-        functions = {},
-        variables = {},
-        -- sidebars = 'transparent',
-        -- floats = 'transparent',
-      },
-    }
-
-    vim.cmd.colorscheme 'tokyonight-night'
-    vim.cmd.hi 'Comment gui=none'
+    -- require('lspconfig.ui.windows').default_options.border = 'rounded'
+    --
+    -- require('tokyonight').setup {
+    --   on_highlights = function(hl)
+    --     hl.TelescopeBorder = {
+    --       fg = '#82A0F1',
+    --     }
+    --     hl.TelescopePromptBorder = {
+    --       fg = '#82A0F1',
+    --     }
+    --     hl.LspInfoBorder = {
+    --       fg = '#82A0F1',
+    --     }
+    --   end,
+    --   -- transparent = true,
+    --   styles = {
+    --     comments = { italic = false },
+    --     keywords = { italic = false },
+    --     functions = {},
+    --     variables = {},
+    --     -- sidebars = 'transparent',
+    --     -- floats = 'transparent',
+    --   },
+    -- }
+    --
+    -- vim.cmd.colorscheme 'tokyonight-night'
+    -- vim.cmd.hi 'Comment gui=none'
   end,
 }
