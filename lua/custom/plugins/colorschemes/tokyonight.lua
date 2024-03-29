7262ba8edfe53d87df2703417133562c862189dd
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-29 23:11:46 +0200

    Removed custom background from tokyonight.

--- lua/custom/plugins/colorschemes/tokyonight.lua
+++ lua/custom/plugins/colorschemes/tokyonight.lua
@@ -3,22 +3,7 @@ return {
   priority = 1000,
   init = function()
     require('tokyonight').setup {
-      on_colors = function(colors)
-        colors.bg = '#0E0F1B'
-        colors.bg_dark = '#0E0F1B'
-        colors.bg_sidebar = '#101425'
-      end,
-
-      on_highlights = function(hl, c)
-        hl.NvimTreeNormal = {
-          bg = c.bg_dark,
-        }
-        hl.NvimTreeNormalNC = {
-          bg = c.bg_dark,
-        }
-        hl.TelescopeNormal = {
-          bg = c.bg_dark,
-        }
+      on_highlights = function(hl)
         hl.TelescopeBorder = {
           fg = '#82A0F1',
         }
