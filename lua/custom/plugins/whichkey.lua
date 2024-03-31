51fd68f57e62e25473a0101a8f07ebaa03bbba4e
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-31 15:08:33 +0300

    Added borders to 'which-key'.

--- lua/custom/plugins/whichkey.lua
+++ lua/custom/plugins/whichkey.lua
@@ -2,7 +2,14 @@ return {
   'folke/which-key.nvim',
   event = 'VimEnter',
   config = function()
-    require('which-key').setup()
+    require('which-key').setup {
+      window = {
+        border = 'rounded',
+        margin = { 0, 0, 1, 0 },
+      },
+      show_help = false,
+    }
+
     require('which-key').register {
       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
