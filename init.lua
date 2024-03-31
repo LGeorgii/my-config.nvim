3cfc6e35f95a1bcd7d61edb617e70a1fd80dfe0e
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-31 14:31:21 +0300

    Formatted 'lazy.lua', 'init.lua'.

--- init.lua
+++ init.lua
@@ -2,14 +2,14 @@ require 'custom.core'
 require 'custom.lazy'
 
 function SetColorScheme(colorScheme)
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.cmd.colorscheme(colorScheme)
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+  vim.cmd.colorscheme(colorScheme)
 
-    require('lualine').setup {
-        options = {
-            theme = colorScheme,
-        },
-    }
+  require('lualine').setup {
+    options = {
+      theme = colorScheme,
+    },
+  }
 end
 
-SetColorScheme('catppuccin')
+SetColorScheme 'catppuccin'
--- lua/custom/lazy.lua
+++ lua/custom/lazy.lua
@@ -13,6 +13,8 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
+vim.keymap.set('n', '<leader>ll', '<cmd>Lazy<CR>', { desc = 'Opens Lazy' })
+
 require('lazy').setup({
   { import = 'custom.plugins' },
   { import = 'custom.colorschemes' },
@@ -25,6 +27,3 @@ require('lazy').setup({
     notify = false,
   },
 })
-
-local keymap = vim.keymap
-keymap.set('n', '<leader>ll', '<cmd>Lazy<CR>', { desc = 'Opens Lazy' })
