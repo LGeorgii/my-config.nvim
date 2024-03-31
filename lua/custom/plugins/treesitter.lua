2b43657d5bc570e0cf3c453cee5ce96fb2c62513
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-03-31 16:54:52 +0300

    Added JSDoc support for 'treesitter'.

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ':TSUpdate',
   config = function()
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { 'javascript', 'typescript', 'html', 'css', 'lua', 'c', 'bash', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'jsdoc', 'javascript', 'typescript', 'html', 'css', 'lua', 'c', 'bash', 'markdown', 'vim', 'vimdoc' },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
