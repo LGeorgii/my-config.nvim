95c6d85389f5a2818c2dd2d4978ae48c2d710e0c
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-04-07 10:57:04 +0300

    Set previous keymaps to jump to autocomplete arguments.

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -62,9 +62,14 @@ return {
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<CR>'] = cmp.mapping.confirm { select = true },
-        ['<Tab>'] = cmp.mapping(function()
+        ['<C-l>'] = cmp.mapping(function()
           if luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
+          end
+        end, { 'i', 's' }),
+        ['<C-h>'] = cmp.mapping(function()
+          if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
           end
         end, { 'i', 's' }),
       },
